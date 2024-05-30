508e4e5ceb7406b000f74cb3a568c79020b9f4eb
zhirongl <zhirong.li@picocom.com>
2024-05-30 12:57:09 +0800

    scbxy: add resize2fs in startup script

--- dynamic-layers/meta-rockchip/recipes-core/picocom-startup-script/files/run-for-picocom.sh
+++ dynamic-layers/meta-rockchip/recipes-core/picocom-startup-script/files/run-for-picocom.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-set -e
-
 case "$1" in
 	start)
 		echo -n "Enabling CPU performance ..."
-		echo performance > /sys/module/pcie_aspm/parameters/policy
+#		echo performance > /sys/module/pcie_aspm/parameters/policy
 		echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 		echo performance > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 		echo performance > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
@@ -13,6 +11,7 @@ case "$1" in
 		echo performance > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
 		echo performance > /sys/devices/system/cpu/cpu5/cpufreq/scaling_governor
 		echo "Creating hugepaga fs ..."
+		resize2fs /dev/mmcblk1p4
 		mkdir -p /mnt/huge32m
 		mount -t hugetlbfs none /mnt/huge32m -o pagesize=32MB
 		echo "Done"
